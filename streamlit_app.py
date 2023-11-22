b758796dca7b6f56dc33fc57832a5fe5f42b8fb3
Hugo Perez <vhpvmx@gmail.com>
2023-11-22 21:32:18 +0100

    Update streamlit_app.py __import__('pysqlite3') import

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,10 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 def generate_response(uploaded_file, openai_api_key, query_text):
     # Load document if file is uploaded
     if uploaded_file is not None:
